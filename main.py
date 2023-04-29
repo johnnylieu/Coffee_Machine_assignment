4958dd8ae5a351c7e4e0e4ee49b52fdd0d4cabb5
Johnny <JohnnyLieu@yahoo.com>
2023-04-28 19:39:32 -0700

    removed comments

--- main.py
+++ main.py
@@ -32,14 +32,6 @@ resources = {
 }
 
 
-# TODO 1. Prompt user by asking "What would you like? (expresso/latte/cappuccino):"
-#  1a: Check the user’s input to decide what to do next.
-#  1b: The prompt should show every time action has completed, e.g. once the drink is dispensed.
-#  The prompt should show again to serve the next customer.
-
-# TODO 2. Turn off the Coffee Machine by entering “off” to the prompt.
-#  2a: For maintainers of the coffee machine, they can use “off” as the secret word to turn off the machine.
-#  Your code should end execution when this happens
 def prompt():
     """prompts the user for which drink they would like. 'off' turns off coffee machine"""
     prompt_response = input(f"What would you like? (expresso/latte/cappuccino): ").lower()
@@ -55,13 +47,6 @@ def prompt():
         return prompt()
 
 
-# TODO 3. Print report
-#  3a. When the user enters “report” to the prompt, a report should be generated that shows
-#  the current resource values. e.g.
-#  Water: 100ml
-#  Milk: 50ml
-#  Coffee: 76g
-#  Money: $2.5
 def print_report():
     """prints available resources"""
     r = resources
@@ -69,12 +54,6 @@ def print_report():
     prompt()
 
 
-# TODO 4. Check resources sufficient?
-#  4a: When the user chooses a drink, the program should check if there are enough
-#  resources to make that drink.
-#  4b: E.g. if Latte requires 200ml water but there is only 100ml left in the machine. It should
-#  not continue to make the drink but print: “Sorry there is not enough water.”
-#  4c: The same should happen if another resource is depleted, e.g. milk or coffee.
 def check_resources(drink):
     """checks if we have enough resources"""
 
@@ -97,13 +76,6 @@ def check_resources(drink):
     prompt()
 
 
-# TODO 5: Process coins
-#  5a: If there are sufficient resources to make the drink selected,
-#  then the program should prompt the user to insert coins.
-#  5b: Remember that quarters = $0.25, dimes = $0.10, nickles = $0.05, pennies = $0.01
-#  5c: Calculate the monetary value of the coins inserted.
-#   E.g. 1 quarter, 2 dimes, 1 nickel, 2 pennies = 0.25 + 0.1 x 2 + 0.05 + 0.01 x 2 = $0.52
-#   round to two decimals
 def insert_coins(drink, water_amount, milk_amount, coffee_amount, water, milk, coffee):
     """returns the total amount that user paid"""
     quarters = int(input("How many quarters?: ")) * 0.25
@@ -118,18 +90,6 @@ def insert_coins(drink, water_amount, milk_amount, coffee_amount, water, milk, c
     check_transaction(total, drink, water_amount, milk_amount, coffee_amount, water, milk, coffee)
 
 
-# TODO 6: Check transaction successful?
-#  6a: Check that the user has inserted enough money to purchase the drink they selected.
-#   E.g Latte cost $2.50, but they only inserted $0.52 then after counting the coins the
-#   program should say “Sorry that's not enough money. Money refunded.”.
-#  6b: But if the user has inserted enough money, then the cost of the drink gets added to the
-#   machine as the profit and this will be reflected the next time “report” is triggered. E.g.
-#       Water: 100ml
-#       Milk: 50ml
-#       Coffee: 76g
-#       Money: $2.5
-#  6c: If the user has inserted too much money, the machine should offer change.
-#   E.g. “Here is $2.45 dollars in change.” The change should be rounded to 2 decimal places.
 def check_transaction(total, drink, water_amount, milk_amount, coffee_amount, water, milk, coffee):
     """checks if transaction is successful"""
     cost = MENU[drink]["cost"]
@@ -147,22 +107,6 @@ def check_transaction(total, drink, water_amount, milk_amount, coffee_amount, wa
         make_coffee(water_amount, milk_amount, coffee_amount, water, milk, coffee)
 
 
-# TODO 7: Make coffee
-#  7a: If the transaction is successful and there are enough resources to make the drink the
-#   user selected, then the ingredients to make the drink should be deducted from the
-#   coffee machine resources.
-#   E.g. report before purchasing latte:
-#       Water: 300ml
-#       Milk: 200ml
-#       Coffee: 100g
-#       Money: $0
-#   Report after purchasing latte:
-#       Water: 100ml
-#       Milk: 50ml
-#       Coffee: 76g
-#       Money: $2.5
-#   7b: Once all resources have been deducted, tell the user “Here is your latte. Enjoy!”.
-#       If latte was their choice of drink.
 def make_coffee(water_amount, milk_amount, coffee_amount, water, milk, coffee):
     """if transaction is successful and there are enough resources, make coffee"""
     money = resources["money"]
